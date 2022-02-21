9f3ca7bdce3bc1000139a1dbbce8f1894ac191c4
rahulvuppla <rahulvuppla@gmail.com>
2022-02-21 05:04:54 -0700

    commit for decrypt failing

--- encrypt.py
+++ encrypt.py
@@ -39,7 +39,7 @@ def decrypt(text,s):
  
         
         elif char.isdigit():
-            result+=str((ord(char)- s-48))
+            result+=str((ord(char)- s-49))
         elif char.islower():
             result += chr((ord(char)  - s- 97) % 26 + 97)
         else:
@@ -57,6 +57,6 @@ class firsttest(unittest.TestCase):
     def test_decrypt(self):
         assert decrypt('23456',1)=='12345'        
 x= firsttest()
-x.test_encrypt() 
+x.test_decrypt() 
 
    
