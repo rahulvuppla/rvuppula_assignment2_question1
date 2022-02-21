71b9d71bced7be8715228c0d7e2010b5184e4f0f
rahulvuppla <rahulvuppla@gmail.com>
2022-02-21 05:01:49 -0700

    This is the 1st encryption commit

--- encrypt.py
+++ encrypt.py
@@ -11,7 +11,7 @@ def encrypt(text,s):
         if (char.isupper()):
             result += chr((ord(char) + s-65) % 26 + 65)
         elif char.isdigit():
-            result+=str((ord(char)+ s-48))    
+            result+=str((ord(char)+ s-49))    
  
         
         elif char.islower():
@@ -22,12 +22,41 @@ def encrypt(text,s):
             result += chr(value % 128)
  
     return result
-   
+
+
+
+def decrypt(text,s):
+    result = ""
+ 
+    
+    for i in range(len(text)):
+        b=text[i]
+        char = b
+ 
+        
+        if (char.isupper()):
+            result += chr((ord(char) - s-65) % 26 + 66)
+ 
+        
+        elif char.isdigit():
+            result+=str((ord(char)- s-48))
+        elif char.islower():
+            result += chr((ord(char)  - s- 97) % 26 + 97)
+        else:
+            value=ord(char)-s
+            result += chr(value % 128)
+            
+ 
+    return result   
 
 class firsttest(unittest.TestCase):
  
     def test_encrypt(self):
-        assert encrypt('12345',1)=='23456c'
+        assert encrypt('12345',1)=='23456'
+
+    def test_decrypt(self):
+        assert decrypt('23456',1)=='12345'        
 x= firsttest()
-x.test_encrypt()    
+x.test_encrypt() 
+
    
